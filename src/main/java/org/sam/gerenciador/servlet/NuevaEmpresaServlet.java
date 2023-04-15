d2f7553544863f1033cc97afa2a21513a60ca18d
Christian <chriscar.sam@gmail.com>
2023-04-15 15:59:03 -0500

    Solo POST

--- src/main/java/org/sam/gerenciador/servlet/NuevaEmpresaServlet.java
+++ src/main/java/org/sam/gerenciador/servlet/NuevaEmpresaServlet.java
@@ -12,7 +12,7 @@ public class NuevaEmpresaServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("nueva empresa registrada");
 		String nombreEmpresa = request.getParameter("nombre");
 		PrintWriter out = response.getWriter();
