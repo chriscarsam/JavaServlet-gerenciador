e3b59ce8b9b4a817bae020a935d71cb88d389326
Christian <chriscar.sam@gmail.com>
2023-04-25 10:09:13 -0500

    Primer filtro

--- src/main/java/org/sam/gerenciador/accion/ListaEmpresas.java
+++ src/main/java/org/sam/gerenciador/accion/ListaEmpresas.java
@@ -15,10 +15,16 @@ public class ListaEmpresas implements Accion{
 
 	public String ejecutar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		
+		long antes = System.currentTimeMillis();
+		
 		DB baseDeDatos = new DB();
 		List<Empresa> listaEmpresas = baseDeDatos.getEmpresas();
 	
 		request.setAttribute("empresas", listaEmpresas);
+		
+		long despues = System.currentTimeMillis();
+		
+		System.out.println("Tiempo de ejecucion: " + (despues - antes));
 		
 		return "forward:listaEmpresas.jsp";
 	}
---
+++ src/main/java/org/sam/gerenciador/servlet/MonitoreoFilter.java
@@ -0,0 +1,30 @@
+package org.sam.gerenciador.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+
+@WebFilter(urlPatterns = "/entrada") // Si ya no quiero utilizar el filtro comento esta línea.
+public class MonitoreoFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		long antes = System.currentTimeMillis();
+		
+		String accion = request.getParameter("accion");
+		
+		//ejecución del controlador
+		chain.doFilter(request, response);
+		
+		long despues = System.currentTimeMillis();
+		System.out.println("Tiempo de ejecucion de la acción: " + accion + " -> " + (despues - antes));
+	}
+
+}
